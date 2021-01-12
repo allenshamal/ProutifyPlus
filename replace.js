d2d2967268a852c8621efceea971b03e004644d0
allenshamal <50488753+allenshamal@users.noreply.github.com>
2021-01-12 23:34:09 +0100

    replace.js

---
+++ replace.js
@@ -0,0 +1,517 @@
+// vim:set sw=2 ts=2 sts=2 expandtab:
+const replace = [
+
+//DROITE  & LREM //
+
+  {
+    "name": "Christine Lagarde",
+    "replaceWith": "Christine Laprout"
+  },
+  {
+    "name": "de Villepin",
+    "replaceWith": "de Villeprout"
+  },
+  {
+    "name": "Balladur",
+    "replaceWith": "Ballaprout"
+  },
+  {
+    "name": "Juppé",
+    "replaceWith": "Juppet"
+  },
+  {
+    "name": "Alliot-Marie",
+    "replaceWith": "Aprout-Marie"
+  },
+  {
+    "name": "Pecresse",
+    "replaceWith": "Petprout"
+  },
+  {
+    "name": "Raffarin",
+    "replaceWith": "Raffapet"
+  },
+  {
+    "name": "Elkabbach",
+    "replaceWith": "Elkaprout"
+  },
+  {
+    "name": "Dati",
+    "replaceWith": "Daprout"
+  },
+  {
+    "name": "Barouin",
+    "replaceWith": "Baprout"
+  },
+  {
+    "name": "Kosciusko-Morizet",
+    "replaceWith": "Kosciuspet-Moriprout"
+  },
+  {
+    "name": "Chevenemant",
+    "replaceWith": "Cheveprout"
+  },
+  {
+    "name": "Guéant",
+    "replaceWith": "Guéprout"
+  },
+  {
+    "name": "Vilani",
+    "replaceWith": "Vilaprout"
+  },
+  {
+    "name": "Manuel Valls",
+    "replaceWith": "Manuel Valls alias Manu le prout"
+  },
+  {
+    "name": "Morano",
+    "replaceWith": "Moraprout"
+  },
+  {
+    "name": "Christine Boutin",
+    "replaceWith": "Christine Boutprout"
+  },
+  {
+    "name": "Fillon",
+    "replaceWith": "Fillpet"
+  },
+  {
+    "name": "Estrosi",
+    "replaceWith": "Estroprout"
+  },
+  {
+    "name": "Chirac",
+    "replaceWith": "Chiprout"
+  },
+
+  {
+    "name": "Darmanin",
+    "replaceWith": "Darmaprout"
+  },
+  {
+    "name": "Bruno Le Maire|Bruno Lemaire",
+    "replaceWith": "Bruno Le Prout"
+  },
+  {
+    "name": "Jean Castex",
+    "replaceWith": "Jean Proutex"
+  },
+  {
+    "name": "Macron|Emmanuel Macron",
+    "replaceWith": "Emmanuel Maprout"
+  },
+  {
+    "name": "Barbara Pompili",
+    "replaceWith": "Barbara Proutili"
+  },
+  {
+    "name": "Jean-Michel Blanquer",
+    "replaceWith": "Jean-Michel Blanc-Prout"
+  },
+  {
+    "name": "Marlène Schiappa",
+    "replaceWith": "Marlène Schiaprout"
+  },
+  {
+    "name": "Élisabeth Borne|Elisabeth Borne",
+    "replaceWith": "Élisabeth Prout"
+  },
+  {
+    "name": "Éric Dupond-Moretti",
+    "replaceWith": "Éric Duprout-Moretti"
+  },
+  {
+    "name": "Gabriel Attal",
+    "replaceWith": "Gabriel Prouttal",
+  },
+  {
+    "name": "Jean-Yves Le Drian|Jean Yves Le Drian|Jean-Yves le Drian",
+    "replaceWith": "Jean-Prout Le Drian"
+  },
+  {
+    "name": "Agnès Pannier-Runacher",
+    "replaceWith": "Agnès Proutier-Runacher"
+  },
+  {
+    "name": "Cédric O|Cedric O",
+    "replaceWith": "Cédric prOut"
+  },
+  {
+    "name": "Florence Parly",
+    "replaceWith": "Florence Proutly"
+  },
+  {
+    "name": "Roselyne Bachelot",
+    "replaceWith": "Proutelyne Bachelot"
+  },
+  {
+    "name": "Olivier Véran",
+    "replaceWith": "Proutivier Véran"
+  },
+  {
+    "name": "d'Olivier Véran",
+    "replaceWith": "de Proutivier Véran"
+  },
+  {
+    "name": "Frédérique Vidal|Frederique Vidal",
+    "replaceWith": "Frédérique Proutal"
+  },
+  {
+    "name": "Amélie de Montchalin|Amélie De Montchalin",
+    "replaceWith": "Amélie de Proutalin"
+  },
+  {
+    "name": "Franck Riester",
+    "replaceWith": "Franck Prouster"
+  },
+  {
+    "name": "Emmanuelle Wargon",
+    "replaceWith": "Emmanuelle Prouton"
+  },
+  {
+    "name": "Jean-Baptiste Djebbari",
+    "replaceWith": "Jean-Baptiste Djeprouti"
+  },
+  {
+    "name": "Olivier Dussopt",
+    "replaceWith": "Olivier Duprout"
+  },
+  {
+    "name": "Geneviève Darrieussecq",
+    "replaceWith": "Geneviève Duproutsecq"
+  },
+  {
+    "name": "Sébastion Lecornu",
+    "replaceWith": "Sébastien Leproutu"
+  },
+  {
+    "name": "Nicolas Sarkozy",
+    "replaceWith": "Nicoprout Sarkozy"
+  },
+  {
+    "name": "Geoffroy Roux de Bézieux",
+    "replaceWith": "Geoffroy Prout de Bézieux"
+  },
+  {
+    "name": "Christian Estrosi",
+    "replaceWith": "Christian Estroprout"
+  },
+  {
+    "name": "Éric Ciotti|Eric Ciotti",
+    "replaceWith": "Éric Proutti"
+  },
+  {
+    "name": "Castaner",
+    "replaceWith": "Castaprout"
+  },
+  {
+    "name": "Edouard Philippe",
+    "replaceWith": "Edouard Philipprout"
+  },
+  {
+    "name": "Gérard Collomb",
+    "replaceWith": "Gérard Collomprout"
+  },
+  {
+    "name": "Agnès Buzin",
+    "replaceWith": "Agnès Buzprout"
+  },
+  {
+    "name": "François de Rugy",
+    "replaceWith": "François de Rugprout"
+  },
+  {
+    "name": "Sibbeth N'Diaye",
+    "replaceWith": "Siprout N'Diaye"
+  },
+  {
+    "name": "Aurore Bergé",
+    "replaceWith": "Auprout Bergé"
+  },
+  {
+    "name": "Muriel Pénicaud",
+    "replaceWith": "Muriel Prouticaud"
+  },
+  {
+    "name": "Nicole Belloubet",
+    "replaceWith": "Nicole Belloupet"
+  },
+
+// EXTREME DROITE //
+ 
+   {
+    "name": "Jordan Bardella",
+    "replaceWith": "Bardellpet"
+  },
+   {
+    "name": "Messina",
+    "replaceWith": "Messiprout"
+  },
+   {
+    "name": "Ménard",
+    "replaceWith": "Méprout"
+  },
+   {
+    "name": "Marion Maréchal",
+    "replaceWith": "Marion Maréchal Le Prout"
+  },
+   {
+    "name": "Philippot",
+    "replaceWith": "Philiprout"
+  },
+   {
+    "name": "Gollnisch",
+    "replaceWith": "Proutnish"
+  },
+   {
+    "name": "Lesquen",
+    "replaceWith": "Lesprout"
+  },
+   {
+    "name": "Soral",
+    "replaceWith": "Solprout"
+  },
+   {
+    "name": "Bel-Ker",
+    "replaceWith": "Bel-Pet"
+  },
+   {
+    "name": "Collard",
+    "replaceWith": "Collprout"
+  },
+   {
+    "name": "Aignant",
+    "replaceWith": "Aiprout"
+  },
+   {
+    "name": "Aliot",
+    "replaceWith": "Alprout"
+  },
+   {
+    "name": "Pen",
+    "replaceWith": "Le prout"
+	
+// CENTRE & DIVERS DROITE //
+
+  },
+   {
+    "name": "Asselineau",
+    "replaceWith": "Asseliprout"
+  },
+   {
+    "name": "Baryou",
+    "replaceWith": "Bayprout"
+  },
+   {
+    "name": "Jean-Frédéric Poisson",
+    "replaceWith": "Jean-Frédéric Poiprout"
+	
+// GAUCHE //
+
+  },
+   {
+    "name": "Fabius",
+    "replaceWith": "Faprout"
+  },
+   {
+    "name": "Jospin",
+    "replaceWith": "Josprout"
+  },
+   {
+    "name": " Ségolène Royal",
+    "replaceWith": "Ségoprout Royal"
+  },
+   {
+    "name": "Jean Luc le Drian",
+    "replaceWith": "Jean Luc Le Prout"
+  },
+   {
+    "name": "Thevenou",
+    "replaceWith": "Theveprout"
+  },
+   {
+    "name": "Cahuzac",
+    "replaceWith": "Cahuprout"
+  },
+   {
+    "name": "Ayrault",
+    "replaceWith": "Ayprout"
+  },
+   {
+    "name": "Hamon",
+    "replaceWith": "Haprout"
+  },
+   {
+    "name": "Poutou",
+    "replaceWith": "Pouprout"
+	
+// FRANCE INSOUMISE
+
+  },
+   {
+    "name": "Quatennens",
+    "replaceWith": "Quateprout"
+  },
+   {
+    "name": "Bompard",
+    "replaceWith": "Bomprout"
+  },
+   {
+    "name": "Corbière",
+    "replaceWith": "Corprout"
+  },
+   {
+    "name": "Ruffin",
+    "replaceWith": "Ruffprout"
+  },
+   {
+    "name": "Mélenchon",
+    "replaceWith": "Mélenprout"
+	},
+   {
+    "name": "Clémentine Autin",
+    "replaceWith": "Clémentine Auprout"
+	},
+   {
+    "name": "Danielle Obono",
+    "replaceWith": "Danielle Oboprout"
+	},
+	
+// ECOLOGIE 
+
+   {
+    "name": "Hulot",
+    "replaceWith": "Hulprout"
+  },
+   {
+    "name": "Jean Vincent Placé",
+    "replaceWith": "Jean Vinpet Plaprout"
+  },
+   {
+    "name": "Mamère",
+    "replaceWith": "Maprout"
+  },
+   {
+    "name": "Eva Joly",
+    "replaceWith": "Eva Joprout"
+  },
+   {
+    "name": "José Bové",
+    "replaceWith": "Jopet Bové"
+  },
+   {
+    "name": "Duflot",
+    "replaceWith": "Dufprout"
+  },
+   {
+    "name": "Daniel Cohn Bandit",
+    "replaceWith": "Daniel Cohn Bandprout"
+  },
+   {
+    "name": "Yannick Jadot",
+    "replaceWith": "Yaprout Jadot"
+  },
+   {
+    "name": "Delphine Batho",
+    "replaceWith": "Delprout Batho"
+  },
+  
+ // DIVERS PERSONNALITÉ //
+ 
+   {
+    "name": "Finkielkraut",
+    "replaceWith": "Finkielprout"
+  },
+    {
+    "name": "Brigitte Macron",
+    "replaceWith": "Brigitte Maprout"
+  },
+    {
+    "name": "Zemmour",
+    "replaceWith": "Zeprout"
+  },
+    {
+    "name": "Pascal Praud",
+    "replaceWith": "Pascal Praud"
+  },
+    {
+    "name": "Morandini",
+    "replaceWith": "Morandiprout"
+  },
+    {
+    "name": "Patrick Cohen",
+    "replaceWith": "Patrick Cohprout"
+  },
+    {
+    "name": "Trump",
+    "replaceWith": "Trumprout"
+  },
+  
+// DIVERS APPELATION //
+
+   {
+    "name": "président|Président",
+    "replaceWith": "proutident"
+  },
+   {
+    "name": "ministre",
+    "replaceWith": "miniprout"
+  },
+   {
+    "name": "dminiprout",
+    "replaceWith": "dministre"
+  },
+   {
+    "name": "député",
+    "replaceWith": "Déprouté"
+  },
+   {
+    "name": "sénateur",
+    "replaceWith": "Sénaprout"
+  },
+   {
+    "name": "plan de relance",
+    "replaceWith": "prout de relance"
+  },
+   {
+    "name": "ce que l'on sait",
+    "replaceWith": "ce que l'on proute"
+  },
+   {
+    "name": "couac",
+    "replaceWith": "prout"
+  },
+   {
+    "name": "Covid",
+    "replaceWith": "Coprout19"
+  },
+   {
+    "name": "Coronavirus",
+    "replaceWith": "Coronaproutus"
+  },
+   {
+    "name": "confinement",
+    "replaceWith": "confiprout"
+  },
+
+];
+
+const rExps = []
+replace.forEach((element) => {
+  rExps.push([new RegExp(element["name"], "g"), element["replaceWith"]])
+})
+
+var textNode;
+let walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+
+// Replace in title
+rExps.forEach(function (rExp) {
+  document.title = document.title.replace(rExp[0], rExp[1]);
+});
+
+// Replace in body
+while (textNode = walk.nextNode()) {
+  rExps.forEach(function (rExp) {
+    textNode.nodeValue = textNode.nodeValue.replace(rExp[0], rExp[1]);
+  });
+}
